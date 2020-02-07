7a86584fd2dbf5232f0d3202551bad388d25dbc6
Manish Agrawal <manish.agrawal@gmail.com>
2020-02-07 08:29:29 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace InClass_TimeComplexity_CSharp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int[] data = { 6, 2, 4, 1, 3, 5 };
+      for (int j = 0; j < data.Length; j++)
+      {
+        int key = data[j];
+        int i = j - 1;
+
+        while ((i >= 0) && (data[i] > key))
+        {
+          data[i + 1] = data[i];
+          i = i - 1;
+        }
+
+        data[i + 1] = key;
+
+        for (int sorted = 0; sorted < data.Length; sorted++)
+        {
+          Console.Write(data[sorted] + " ");
+        }
+        Console.Write("\n");
+      }
+    }
+  }
+}
